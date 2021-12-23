78b8ed325ccdea2cd403091b8c528960304b8c99
dinesh_clinton <dinesh_clinton@trimble.com>
2021-12-23 17:09:19 +0530

    Dinesh L4 End

--- main.js
+++ main.js
@@ -2,7 +2,11 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            image: './assets/images/socks_blue.jpg'
+            image: './assets/images/socks_blue.jpg',
+            inStock: true,
+            isDiscounted: true,
+            inventory: 0,
+            onSale: true
         }
     }
 })
