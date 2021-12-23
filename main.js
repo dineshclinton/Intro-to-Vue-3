2362f34dfeb43e7f9888b4767a3cfef997869cc8
dinesh_clinton <dinesh_clinton@trimble.com>
2021-12-23 16:40:27 +0530

    Dinesh L2 End

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Shoes',
+            description: 'This Shoes are the ones preferred by everyone'
+        }
+    }
+})
