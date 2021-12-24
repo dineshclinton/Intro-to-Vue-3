577b4625044b8a83628882fa9f5f20df035c08bd
dinesh_clinton <dinesh_clinton@trimble.com>
2021-12-24 14:28:20 +0530

    Dinesh L8 End

--- main.js
+++ main.js
@@ -4,12 +4,12 @@ const app = Vue.createApp({
             cart:0,
             product: 'Socks',
             brand: 'Vue Mastery',
-            image: './assets/images/socks_blue.jpg',
-            inStock: false,
             details: ['50% cotton', '30% wool', '20% polyester'],
+            onSale : true,
+            selectedIndex: 0,
             variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
             ]
         }
     },
@@ -19,6 +19,24 @@ const app = Vue.createApp({
         },
         updateImage(variantImage) {
             this.image = variantImage
+        },
+        updateVariant(index) {
+            this.selectedIndex = index;
         }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product; //+ (this.onSale? ' is on sale' : '')
+        },
+        image() {
+            return this.variants[this.selectedIndex].image;
+        },
+        inStock() {
+            return this.variants[this.selectedIndex].quantity;
+        },
+        sale() {
+            return this.onSale? this.brand + ' ' + this.product + ' is on sale' : '';
+        }
+    
     }
 })
